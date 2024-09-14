0721e89e7c8a071f084a5d7c5ff98933aa7ed447
l1ndsay05 <lml9352@nyu.edu>
2024-09-13 23:14:10 -0400

    Update

--- Week01/MyPlayground.playground/Contents.swift
+++ Week01/MyPlayground.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func printChar(length: Int){
+    var mult = Int(2)
+    for i in 1...length{
+        if i.isMultiple(of: mult){
+            print("👾", terminator: "")
+        } else {
+            print("🤡", terminator: "")
+        }
+    }
+}
+
+printChar(length: 11)

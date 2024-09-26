56e9840b881938110d92cb5282dd35a1f4446694
l1ndsay05 <lml9352@nyu.edu>
2024-09-26 16:57:07 -0400

    uodate

---
+++ week03/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by linds on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
